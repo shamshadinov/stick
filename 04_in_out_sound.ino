8a42de5c91b220889f96a8a7a9d36294ec15e74d
Artur <shamshadinov@gmail.com>
2014-06-08 03:21:05 +0400

    04_in_out_sound_INFO
    Дисторсия. Мы упёрлись в ограничение железа. Ардуино не выдаёт более качественный звук.
    Но решили добавить эффект дисторсии. Десятый пин - кнопка включения дисторсии.
    Дисторсия написана строкой:
    sensorValue=(1+0.001*random(1,100))*(sensorValue+0.66*sensorPrev+0.33*sensorPrev2)/2;
    Железо сейчас выглядит так:
    http://cs616230.vk.me/v616230330/11937/QuINS_QC17A.jpg

--- 04_in_out_sound.ino
+++ 04_in_out_sound.ino
@@ -1,23 +1,37 @@
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = 9; // Analog output pin that the LED is attached to
+const int analogInPin = A0;  // Analog input pin that the guitar is attached to
+
 
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
+int sensorPrev = 0;
+int sensorPrev2 = 0;
+int buttonState = 0;
 int k=1;
 int ki=1;
+void setup() {
 
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(10, INPUT);
+}
 
 void loop() {
   
-  sensorValue = 3*analogRead(analogInPin);            
 
+sensorValue = analogRead(analogInPin);      
 
-/*if (sensorValue>128) {  
-  Serial.print("sensor = " );                       
-  Serial.println(sensorValue);      
-}
-
-*/
+    
+sensorPrev2=sensorPrev;
+sensorPrev=sensorValue;
+buttonState = digitalRead(10);
+if (buttonState) sensorValue=(1+0.001*random(1,100))*(sensorValue+0.66*sensorPrev+0.33*sensorPrev2)/2;
+else sensorValue=(sensorValue+sensorPrev+sensorPrev2)/3;
+//delay (3);
 if (sensorValue%2) digitalWrite(2,HIGH);
 else digitalWrite(2,LOW);
 sensorValue=sensorValue/2;
@@ -45,13 +59,5 @@ sensorValue=sensorValue/2;
 
 if (sensorValue%2) digitalWrite(8,HIGH);
 else digitalWrite(8,LOW);
-sensorValue=sensorValue/2;
-
-/*  Serial.print("sensor = " );                       
-
-  Serial.print(sensorValue);      
-  Serial.print("\t output = ");      
-  Serial.println(sensorValue);   
-*/
 
 }
